93529aa547965c099507b2e5ea92ff7a964fea0a
Flavio Ferreira <flavio_luiz_ferreira@hotmail.com>
2025-06-09 11:56:30 -0300

    Add Playwright testing framework and initial test setup; update ESLint config and .gitignore

---
+++ tests-e2e/pokedex.test.js
@@ -0,0 +1,14 @@
+// filepath: tests-e2e/pokedex.test.js
+import { test, expect } from "@playwright/test";
+
+test.describe("Pokedex", () => {
+  test("front page can be opened", async ({ page }) => {
+    await page.goto("");
+    await expect(page.getByText("ivysaur")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo."
+      )
+    ).toBeVisible();
+  });
+});
