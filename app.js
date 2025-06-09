ce38d3fed8cfde6dded1d9b3833e27f6e94b4732
Flavio Ferreira <flavio_luiz_ferreira@hotmail.com>
2025-06-09 14:56:51 -0300

    Refactor app.js for improved code formatting and add render deployment workflow

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-console */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
+
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
+
+// This should come last:
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
